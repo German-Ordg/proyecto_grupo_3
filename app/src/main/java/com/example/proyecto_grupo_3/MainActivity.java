78699075bf473cff210112d4fbe2574a9c5761d6
German-Ordg <gerd_ordonezg@unicah.edu>
2022-03-16 11:44:48 -0600

    creacion de un nuevo value para poner un spinner(COMBOBOX)

--- app/src/main/java/com/example/proyecto_grupo_3/MainActivity.java
+++ app/src/main/java/com/example/proyecto_grupo_3/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.proyecto_grupo_3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
+    Spinner Combo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Combo= (Spinner) findViewById(R.id.spinnerCombo);
+
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,
+                R.array.Combo, android.R.layout.simple_spinner_item);
+        Combo.setAdapter(adapter);
+
     }
 }
