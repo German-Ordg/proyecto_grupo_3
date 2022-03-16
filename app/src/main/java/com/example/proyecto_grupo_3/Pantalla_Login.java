38573d0956d403e60f906a69020038b5dde118e4
German-Ordg <gerd_ordonezg@unicah.edu>
2022-03-16 00:15:00 -0600

    creacion de todas las pantallas

---
+++ app/src/main/java/com/example/proyecto_grupo_3/Pantalla_Login.java
@@ -0,0 +1,14 @@
+package com.example.proyecto_grupo_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pantalla_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_login);
+    }
+}
---
+++ app/src/main/java/com/example/proyecto_grupo_3/Pantalla_Pedido_Detalle.java
@@ -0,0 +1,14 @@
+package com.example.proyecto_grupo_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pantalla_Pedido_Detalle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_pedido_detalle);
+    }
+}
