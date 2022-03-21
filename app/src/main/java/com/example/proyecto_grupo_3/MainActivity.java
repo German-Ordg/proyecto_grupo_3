c387706afd23b7926b4b3d7f466f91bebd87d83f
German-Ordg <gerd_ordonezg@unicah.edu>
2022-03-20 20:25:53 -0600

    s

--- app/src/main/java/com/example/proyecto_grupo_3/MainActivity.java
+++ app/src/main/java/com/example/proyecto_grupo_3/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Combo= (Spinner) findViewById(R.id.combobox);
+
 
         ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,
                 R.array.Combo, android.R.layout.simple_spinner_item);
