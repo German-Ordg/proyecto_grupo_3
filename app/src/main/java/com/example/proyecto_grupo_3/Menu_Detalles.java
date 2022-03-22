d615a21ca310785302eb388f9485ebdb7997a2f3
lesnin <lesramirezc@gmail.com>
2022-03-21 21:31:36 -0600

    Pantalla Nueva, Menu Detalles

---
+++ app/src/main/java/com/example/proyecto_grupo_3/Menu_Detalles.java
@@ -0,0 +1,13 @@
+package com.example.proyecto_grupo_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Menu_Detalles extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_detalles);
+    }
+}
