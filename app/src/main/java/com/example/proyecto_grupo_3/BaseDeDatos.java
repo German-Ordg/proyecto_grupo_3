a5656224f7e9dfaecbe82a023cdf7908c96de488
German-Ordg <gerd_ordonezg@unicah.edu>
2022-03-23 02:12:57 -0600

    creacion de base de datos

---
+++ app/src/main/java/com/example/proyecto_grupo_3/BaseDeDatos.java
@@ -0,0 +1,57 @@
+package com.example.proyecto_grupo_3;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class BaseDeDatos extends SQLiteOpenHelper {
+
+
+    public BaseDeDatos(@Nullable Context context, @Nullable String name,
+                       @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    //metodo create
+    @Override
+    public void onCreate(SQLiteDatabase BaseDeDatos) {
+        BaseDeDatos.execSQL("create table Codigo_qr(numero_mesa integer PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                "codigo_qr text not null)");
+        BaseDeDatos.execSQL("create table Categoria_Producto(codigo_categoria integer PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                "descripcion_categoria text not null)");
+        BaseDeDatos.execSQL("create table Productos(codigo_producto integer PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                "codigo_categoria integer not null, descripcion_producto text not null, precio_actual real not null," +
+                " foreign key(codigo_categoria) references Categoria_Producto(codigo_categoria))");
+        BaseDeDatos.execSQL("create table Ordenes(codigo_orden integer PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                "codigo_producto integer not null, cantidad integer not null, precio real not null," +
+                " masa text not null, tamaño text not null," +
+                " foreign key(codigo_producto) references Productos(codigo_producto))");
+        BaseDeDatos.execSQL("create table Estado(codigo_estado integer primary key," +
+                "descripcion_estado text not null)");
+        BaseDeDatos.execSQL("create table Empleado_Puesto(codigo_puesto integer primary key," +
+                "descripcion_puesto text not null)");
+        BaseDeDatos.execSQL("create table Empleados(codigo_empleado integer primary key," +
+                "codigo_puesto integer not null,nombre_empleado text not null,apellido_empleado text not null," +
+                "numero_identidad_empleado text not null," +
+                "fecha_nacimiento text,fecha_ingreso text not null,num_telefono text,genero text," +
+                "foreign key(codigo_puesto)references Empleado_Puesto(codigo_puesto))");
+        BaseDeDatos.execSQL("create table Usuario(nombre_usuario text primary key," +
+                "codigo_empleado integer not null,codigo_estado integer not null," +
+                "contrasena text not null,correo_electronico text not null," +
+                "foreign key (codigo_empleado) references Empleados(codigo_empleado)," +
+                "foreign key (codigo_estado) references Estado(codigo_estado))");
+        BaseDeDatos.execSQL("create table Pedidos(codigo_pedido integer PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                "codigo_orden integer not null, codigo_estado integer not null, numero_mesa integer not null," +
+                " foreign key(codigo_estado) references Estado(codigo_estado)," +
+                "foreign key(numero_mesa) references Codigo_qr(numero_mesa)," +
+                "foreign key(codigo_orden) references Ordenes(codigo_orden))");
+    }
+
+    //metodo upgrade
+    @Override
+    public void onUpgrade(SQLiteDatabase BaseDeDatos, int OldVersion, int NewVersion) {
+
+    }
+}
--- app/src/main/java/com/example/proyecto_grupo_3/Pantalla_Pedidos.java
+++ app/src/main/java/com/example/proyecto_grupo_3/Pantalla_Pedidos.java
@@ -2,13 +2,45 @@ package com.example.proyecto_grupo_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Pantalla_Pedidos extends AppCompatActivity {
 
+    private EditText ob_qr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_pedidos);
+
+        //variables objetos xml a java
+        ob_qr=(EditText) findViewById(R.id.txtqr);
+
+    }
+
+    public void ingresar(View view){
+        BaseDeDatos admin = new BaseDeDatos(this,"administrador",null,1);
+        SQLiteDatabase BaseDatos = admin.getWritableDatabase();
+        String codigo= ob_qr.getText().toString();
+
+
+        if (!codigo.isEmpty()) {
+
+            ContentValues registro = new ContentValues();//Contenedor de registro
+            registro.put("codigo", codigo);//insertar codigo en la tabla
+
+
+            Toast.makeText(this, "Datos guardados", Toast.LENGTH_SHORT).show();
+            BaseDatos.insert("Codigo_qr", null, registro);
+            BaseDatos.close();
+
+        } else{
+            Toast.makeText(this, "Todos los campus deben llenarse", Toast.LENGTH_SHORT).show();
+        }
+        ob_qr.setText("");
     }
 }
