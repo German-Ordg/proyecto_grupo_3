274e7ab196bf7344f8b4bb68df4c03d03da2e608
German-Ordg <gerd_ordonezg@unicah.edu>
2022-03-23 17:51:34 -0600

    Union rama lesnin

--- app/src/main/java/com/example/proyecto_grupo_3/MainActivity.java
+++ app/src/main/java/com/example/proyecto_grupo_3/MainActivity.java
@@ -12,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Spinner Combo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
