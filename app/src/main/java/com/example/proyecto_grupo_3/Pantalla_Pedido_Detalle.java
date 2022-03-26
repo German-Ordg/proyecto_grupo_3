9451ee8d37c0afcdcaff918a50870b382fbdc52a
German-Ordg <gerd_ordonezg@unicah.edu>
2022-03-26 12:07:41 -0600

    cambios pantalla pedido

--- app/src/main/java/com/example/proyecto_grupo_3/Pantalla_Pedido_Detalle.java
+++ app/src/main/java/com/example/proyecto_grupo_3/Pantalla_Pedido_Detalle.java
@@ -2,7 +2,9 @@ package com.example.proyecto_grupo_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Pantalla_Pedido_Detalle extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class Pantalla_Pedido_Detalle extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_pedido_detalle);
+
+        Intent intent = getIntent();
+
+        int mesa1 = intent.getIntExtra(Pantalla_Pedidos.MESA1,-1);
+
+
+        Toast.makeText(this,"Mesa: "+mesa1, Toast.LENGTH_SHORT).show();
+
     }
 }
--- app/src/main/java/com/example/proyecto_grupo_3/Pantalla_Pedidos.java
+++ app/src/main/java/com/example/proyecto_grupo_3/Pantalla_Pedidos.java
@@ -3,44 +3,58 @@ package com.example.proyecto_grupo_3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class Pantalla_Pedidos extends AppCompatActivity {
 
-    private EditText ob_qr;
+    public static final String MESA1="mesa1";
+    private Button boton1, boton2, boton3, boton4;
+    int mesa1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_pedidos);
+        boton1 = (Button) findViewById(R.id.btnmes1);
+        boton2 = (Button) findViewById(R.id.btnmes2);
+        boton3 = (Button) findViewById(R.id.btnmes3);
+        boton4 = (Button) findViewById(R.id.btnmes4);
+        boton1.setOnClickListener((view) ->{
+            Intent intent = new Intent(this,Pantalla_Pedido_Detalle.class) ;
+            mesa1=1;
+            intent.putExtra(MESA1,mesa1);
+            startActivity(intent);
 
-        //variables objetos xml a java
-        ob_qr=(EditText) findViewById(R.id.txtqr);
+        });
+        boton2.setOnClickListener((view) ->{
+            Intent intent = new Intent(this,Pantalla_Pedido_Detalle.class) ;
+            mesa1=2;
+            intent.putExtra(MESA1,mesa1);
+            startActivity(intent);
 
+        });
+        boton3.setOnClickListener((view) ->{
+            Intent intent = new Intent(this,Pantalla_Pedido_Detalle.class) ;
+            mesa1=3;
+            intent.putExtra(MESA1,mesa1);
+            startActivity(intent);
+
+        });
+        boton4.setOnClickListener((view) ->{
+            Intent intent = new Intent(this,Pantalla_Pedido_Detalle.class) ;
+            mesa1=4;
+            intent.putExtra(MESA1,mesa1);
+            startActivity(intent);
+
+        });
     }
 
-    public void ingresar(View view){
-        BaseDeDatos admin = new BaseDeDatos(this,"administrador",null,1);
-        SQLiteDatabase BaseDatos = admin.getWritableDatabase();
-        String codigo= ob_qr.getText().toString();
 
 
-        if (!codigo.isEmpty()) {
 
-            ContentValues registro = new ContentValues();//Contenedor de registro
-            registro.put("codigo_qr", codigo);//insertar codigo en la tabla
-
-
-            Toast.makeText(this, "Datos guardados", Toast.LENGTH_SHORT).show();
-            BaseDatos.insert("Codigo_qr", null, registro);
-            BaseDatos.close();
-
-        } else{
-            Toast.makeText(this, "Todos los campus deben llenarse", Toast.LENGTH_SHORT).show();
-        }
-        ob_qr.setText("");
-    }
 }
